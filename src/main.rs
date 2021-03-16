3eaf9eb98a8b3804dbc233f95baa4b7eb65255a7
Matthew Kehrt <mkehrt@roseandsigil.net>
2021-03-16 01:34:58 -0700

    Rust start.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use {Serde, Argh, std::PathBuf}
+
+#[derive()Argh::fromArgs)]
+struct Args {
+  prelude: PathBuf,
+  words: PathBuf,
+  postlude: PathBuf,
+  output: PathBuf,
+}
+
+#[derive(Serde::Deserialize)]
+struct Entry {
+  word: String,
+  pos: String,
+  defs: Vec<String>
+  etym: String
+  notes: Option<String>
+}
+
 fn main() {
     println!("Hello, world!");
 }
