83e57b7306ab0fe5c9c50f2ad9d60791cb6d69b5
Matthew Kehrt <mkehrt@roseandsigil.net>
2021-03-21 13:06:25 -0700

    Rust wip.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use {Serde, Argh, std::PathBuf}
+use {Serde, Argh, std::{io::File, pathbuf::PathBuf)}
 
 #[derive()Argh::fromArgs)]
 struct Args {
@@ -17,6 +17,15 @@ struct Entry {
   notes: Option<String>
 }
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> io::Result<()>) {
+    let args = Args::from_env());
+    
+    let mut prelude = File::open(args.prelude)?;
+    let mut words = File::open(args.words)?;
+    let mut postlude = File::open(args.postlude)?;
+    let mut output = File::create(args.output)?;
+
+    output.write_all(prelude)?;
+
+    output.write_all(postlude)?;
 }
