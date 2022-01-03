6d6ab0e7b82403448080ada8b9f69877901a0bdd
Matthew Kehrt <mkehrt@roseandsigil.net>
2022-01-03 14:35:19 -0800

    Litearlly swap mains

--- src/convert/main.rs
+++ src/convert/main.rs
@@ -1,33 +1,51 @@
 use {
     argh,
-    std::fs::File, std::io::Write as _, std::path::PathBuf,
+    std::fs::File, std::io, std::io::Write as _, std::path::PathBuf,
     lexicon::error,
     lexicon::serde,
+    lexicon::types,
 };
 
 #[derive(argh::FromArgs)]
 /// Convert a lexicon YAML file to LaTeX
 struct Args {
+    /// file containing prelude LaTeX.
+    #[argh(option)]
+    prelude: PathBuf,
     /// file containing YAML.
     #[argh(option)]
     words: PathBuf,
+    /// file containing postlude LaTeX.
+    #[argh(option)]
+    postlude: PathBuf,
+    /// output LaTeX file.
+    #[argh(option)]
+    output: PathBuf,
+}
+
+fn report_invalid_entries(entries: &Vec<serde::Entry>) {
+    eprintln!("The following lexicon entries were not complete and could not be output:");
+    for entry in entries {
+        eprintln!("{:?}", entry);
+    }
 }
 
 fn main() -> Result<(), error::LexiconError> {
     let args: Args = argh::from_env();
-    
-    let mut words_file = File::open(&args.words)?;
-    let mut entries = serde::serde_entries_from_reader(&mut words_file)?;
-    drop(words_file);
 
-    entries.sort();
+    let mut prelude_file = File::open(args.prelude)?;
+    let mut words_file = File::open(args.words)?;
+    let mut postlude_file = File::open(args.postlude)?;
+    let mut output_file = File::create(args.output)?;
 
-    let mut words_file = File::create(&args.words)?;
-    for entry in entries {
-        let yaml_entry =  serde_yaml::to_string(&entry)?;
-        writeln!(words_file, "{:}", yaml_entry)?;
-        writeln!(words_file, "",)?;
-    }
+    let entries = serde::entries_from_reader(&mut words_file)?;
+    let sections = types::Sections::new_from_entries(entries.valid);
+
+    report_invalid_entries(&entries.invalid);
+
+    io::copy(&mut prelude_file, &mut output_file)?;
+    write!(output_file, "{:}", sections)?;
+    io::copy(&mut postlude_file, &mut output_file)?;
 
     Ok(())
 }
